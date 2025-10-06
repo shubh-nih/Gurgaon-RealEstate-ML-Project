16092ff9bf67b64d6877ac32b9590ed91ebc5839
Shubham Bisht <shubhambisht149@gmail.com>
2025-10-06 07:33:12 +0530

    add new viz

--- pages/2-Analysis.py
+++ pages/2-Analysis.py
@@ -5,11 +5,13 @@ import pandas as pd
 from wordcloud import WordCloud
 import pickle
 import seaborn as sns
+
 st.set_page_config(page_title = 'Analysis Module', 
                    layout = 'wide')
 
 df = pd.read_csv(r'Datasets\gurgaon-data-viz.csv')
 text = pickle.load(open(r'Datasets\feature_text.pkl', 'rb'))
+featured_df = pickle.load(open(r'Datasets\features_df.pkl', 'rb'))
 
 st.markdown("<h3 style='text-align: center;color: white; font-size: 48px; font-family: Modern Sans-Serif;'>Gurgaon Real Estate Analysis</h3>", unsafe_allow_html=True)
 
@@ -28,19 +30,45 @@ st.plotly_chart(fig, use_container_width = True)
 st.markdown('---')
 
 st.header('Features Wordcloud')
-wordcloud = WordCloud(
-    width = 800, 
-    height = 800, 
-    background_color = 'black', 
-    stopwords = set(['s']), 
-    min_font_size = 10
-).generate(text)
 
-fig2, ax = plt.subplots(figsize=(8, 8), facecolor = 'black')
-ax.imshow(wordcloud, interpolation = 'bilinear')
-ax.axis("off")
-plt.tight_layout(pad = 0)
-st.pyplot(fig2)
+sector_name = st.selectbox('Select Sector', 
+            sorted(featured_df['sector'].unique().tolist()),
+            index = None, 
+            placeholder = 'Overall features')
+
+if sector_name:
+
+    features_list = featured_df.loc[featured_df['sector'] == sector_name, 'features'].values[0]
+    text = " ".join(features_list)
+
+    wordcloud = WordCloud(
+        width = 800, 
+        height = 800, 
+        background_color = 'black', 
+        stopwords = set(['s']), 
+        min_font_size = 10
+    ).generate(text)
+
+    fig2, ax = plt.subplots(figsize=(8, 8), facecolor = 'black')
+    ax.imshow(wordcloud, interpolation = 'bilinear')
+    ax.axis("off")
+    plt.tight_layout(pad = 0)
+    st.pyplot(fig2)
+    
+else:
+    wordcloud = WordCloud(
+        width = 800, 
+        height = 800, 
+        background_color = 'black', 
+        stopwords = set(['s']), 
+        min_font_size = 10
+    ).generate(text)
+
+    fig2, ax = plt.subplots(figsize=(8, 8), facecolor = 'black')
+    ax.imshow(wordcloud, interpolation = 'bilinear')
+    ax.axis("off")
+    plt.tight_layout(pad = 0)
+    st.pyplot(fig2)
 
 st.markdown('---')
 
@@ -51,11 +79,11 @@ if property_type == 'House':
     fig3 = px.scatter(df[df['property_type'] == 'house'], x = 'built_up_area', y = 'price', color = 'bedRoom')
     st.plotly_chart(fig3, use_container_width = True)
 elif property_type == 'Flat':
-    fig4 = px.scatter(df[df['property_type'] == 'flat'], x = 'built_up_area', y = 'price', color = 'bedRoom')
-    st.plotly_chart(fig4, use_container_width = True)
+    fig3 = px.scatter(df[df['property_type'] == 'flat'], x = 'built_up_area', y = 'price', color = 'bedRoom')
+    st.plotly_chart(fig3, use_container_width = True)
 else:
-    fig5 = px.scatter(df, x = 'built_up_area', y = 'price', color = 'bedRoom')
-    st.plotly_chart(fig5, use_container_width = True)
+    fig3 = px.scatter(df, x = 'built_up_area', y = 'price', color = 'bedRoom')
+    st.plotly_chart(fig3, use_container_width = True)
     
 st.markdown('---')
 
@@ -64,25 +92,27 @@ option = sorted(df['sector'].unique().tolist())
 selected_sector = st.selectbox('Select Sector', option, index = None, placeholder = 'Select an Option')
 
 if selected_sector:
-    fig6 = px.pie(df[df['sector'] == selected_sector], names = 'bedRoom')
-    st.plotly_chart(fig6, use_container_width = True)
+    fig4 = px.pie(df[df['sector'] == selected_sector], names = 'bedRoom')
+    st.plotly_chart(fig4, use_container_width = True)
 else:
-    fig7 = px.pie(df, names = 'bedRoom')
-    st.plotly_chart(fig7, use_container_width = True)
+    fig4 = px.pie(df, names = 'bedRoom')
+    st.plotly_chart(fig4, use_container_width = True)
     
 st.markdown('---')
 
 st.header('Side by Side BHK price comparison')
 
-fig8 = px.box(df[df['bedRoom'] <= 4], x = 'bedRoom', y = 'price', title = 'BHK Price Range')
-st.plotly_chart(fig8, use_container_width = True)
+fig5 = px.box(df[df['bedRoom'] <= 4], x = 'bedRoom', y = 'price', title = 'BHK Price Range')
+st.plotly_chart(fig5, use_container_width = True)
 
 st.markdown('---')
 
 st.header('Side by Side Distplot for property type')
 
-fig9 = plt.figure(figsize=(10, 4))
+fig6 = plt.figure(figsize=(10, 4))
 sns.histplot(df[df['property_type'] == 'house']['price'], label='House', kde=True, color='blue')
 sns.histplot(df[df['property_type'] == 'flat']['price'], label='Flat', kde=True, color='orange')
 plt.legend()
-st.pyplot(fig9)
+st.pyplot(fig6)
+
+st.markdown('---')
